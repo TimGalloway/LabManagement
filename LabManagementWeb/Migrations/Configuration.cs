b363de635797de8071cf97738cb01293df19c31b
Tim Galloway <galloway@iinet.net.au>
2018-10-15 19:44:45 +0800

    Got published to windows server machine

--- LabManagementWeb/Migrations/Configuration.cs
+++ LabManagementWeb/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace LabManagementWeb.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(LabManagementWeb.Models.ApplicationDBContext context)
