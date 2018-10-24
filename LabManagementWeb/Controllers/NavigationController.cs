1d5dc4431ce60ce7d42299ca2f72e324700cf5cd
Tim Galloway <galloway@iinet.net.au>
2018-10-24 22:13:51 +0800

    Layout looks OK

---
+++ LabManagementWeb/Controllers/NavigationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabManagementWeb.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navbar
+        public ActionResult TopNav()
+        {
+            var nav = new Navbar();
+            return PartialView("_topNav", nav.NavbarTop());
+        }
+    }
+}
